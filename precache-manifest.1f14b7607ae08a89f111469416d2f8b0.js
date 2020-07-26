9c7a84f8fbd9c77b0849d31b39a96e5f0d939df4
Vladyslav_Shynder <shynderv@gmail.com>
2020-07-26 20:24:00 +0300

    Updates

---
+++ precache-manifest.1f14b7607ae08a89f111469416d2f8b0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ecc5dcee5b4aa3db8ae8c3c3bd319fe",
+    "url": "/hw-react-01/index.html"
+  },
+  {
+    "revision": "d78a4358043a3efcdcfb",
+    "url": "/hw-react-01/static/css/main.f98826b6.chunk.css"
+  },
+  {
+    "revision": "6a1dac36fc2a00303511",
+    "url": "/hw-react-01/static/js/2.f56ec47c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/hw-react-01/static/js/2.f56ec47c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d78a4358043a3efcdcfb",
+    "url": "/hw-react-01/static/js/main.a32b96d9.chunk.js"
+  },
+  {
+    "revision": "b3b58c44d86cd8f4b87a",
+    "url": "/hw-react-01/static/js/runtime-main.6ff29196.js"
+  }
+]);
